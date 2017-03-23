e5132a7210e3f4ddca805594cb8862dbe06bedb2
Brendan <brendan.schindler@sjsu.edu>
2017-03-23 15:23:48 -0700

    Welcome Page Created

---
+++ Library/Library/WelcomePage.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WelcomePage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnReview = New System.Windows.Forms.Button()
+        Me.btnLogout = New System.Windows.Forms.Button()
+        Me.lblWelcome = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnReview
+        '
+        Me.btnReview.Location = New System.Drawing.Point(513, 295)
+        Me.btnReview.Name = "btnReview"
+        Me.btnReview.Size = New System.Drawing.Size(155, 87)
+        Me.btnReview.TabIndex = 28
+        Me.btnReview.Text = "Account Review"
+        Me.btnReview.UseVisualStyleBackColor = True
+        '
+        'btnLogout
+        '
+        Me.btnLogout.Location = New System.Drawing.Point(163, 295)
+        Me.btnLogout.Name = "btnLogout"
+        Me.btnLogout.Size = New System.Drawing.Size(155, 87)
+        Me.btnLogout.TabIndex = 27
+        Me.btnLogout.Text = "Logout"
+        Me.btnLogout.UseVisualStyleBackColor = True
+        '
+        'lblWelcome
+        '
+        Me.lblWelcome.AutoSize = True
+        Me.lblWelcome.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWelcome.Location = New System.Drawing.Point(275, 112)
+        Me.lblWelcome.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblWelcome.Name = "lblWelcome"
+        Me.lblWelcome.Size = New System.Drawing.Size(231, 55)
+        Me.lblWelcome.TabIndex = 26
+        Me.lblWelcome.Text = "Welcome"
+        Me.lblWelcome.UseMnemonic = False
+        '
+        'WelcomePage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(830, 494)
+        Me.Controls.Add(Me.btnReview)
+        Me.Controls.Add(Me.btnLogout)
+        Me.Controls.Add(Me.lblWelcome)
+        Me.Name = "WelcomePage"
+        Me.Text = "WelcomePage"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnReview As Button
+    Friend WithEvents btnLogout As Button
+    Friend WithEvents lblWelcome As Label
+End Class
