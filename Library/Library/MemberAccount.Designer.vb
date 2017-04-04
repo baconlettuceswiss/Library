dd18b610dec072fe0ecb08b7b7ce05e38972ff62
Brendan <brendan.schindler@sjsu.edu>
2017-04-04 12:35:01 -0700

    Member Account Form Created

---
+++ Library/Library/MemberAccount.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MemberAccount
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Title = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Author = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Genre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CheckoutDate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ReturnDate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Title, Me.Author, Me.Genre, Me.CheckoutDate, Me.ReturnDate})
+        Me.DataGridView1.Location = New System.Drawing.Point(68, 131)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(686, 254)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Title
+        '
+        Me.Title.HeaderText = "Tilte"
+        Me.Title.Name = "Title"
+        Me.Title.Width = 74
+        '
+        'Author
+        '
+        Me.Author.HeaderText = "Author"
+        Me.Author.Name = "Author"
+        Me.Author.Width = 93
+        '
+        'Genre
+        '
+        Me.Genre.HeaderText = "Genre"
+        Me.Genre.Name = "Genre"
+        Me.Genre.Width = 90
+        '
+        'CheckoutDate
+        '
+        Me.CheckoutDate.HeaderText = "Checkout Date"
+        Me.CheckoutDate.Name = "CheckoutDate"
+        Me.CheckoutDate.Width = 140
+        '
+        'ReturnDate
+        '
+        Me.ReturnDate.HeaderText = "Return Date"
+        Me.ReturnDate.Name = "ReturnDate"
+        Me.ReturnDate.Width = 123
+        '
+        'MemberAccount
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(945, 469)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "MemberAccount"
+        Me.Text = "MemberAccount"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Title As DataGridViewTextBoxColumn
+    Friend WithEvents Author As DataGridViewTextBoxColumn
+    Friend WithEvents Genre As DataGridViewTextBoxColumn
+    Friend WithEvents CheckoutDate As DataGridViewTextBoxColumn
+    Friend WithEvents ReturnDate As DataGridViewTextBoxColumn
+End Class
---
+++ Library/Library/MemberAccount.vb
@@ -0,0 +1,3 @@
+﻿Public Class MemberAccount
+
+End Class
