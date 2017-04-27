bb0b3c22c9a719049a7fde32217e6ec17fa89f8b
Brendan Schindler <brendan.schindler@sjsu.edu>
2017-04-26 17:31:28 -0700

    working on getting member search to work again after I lost all my data

--- Library/Library/MemberSearch.Designer.vb
+++ Library/Library/MemberSearch.Designer.vb
@@ -22,23 +22,35 @@ Partial Class MemberSearch
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.AuthorSearch = New System.Windows.Forms.Button()
         Me.TitleSearch = New System.Windows.Forms.Button()
         Me.AuthorTB = New System.Windows.Forms.TextBox()
         Me.TitleTB = New System.Windows.Forms.TextBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.SearchResults = New System.Windows.Forms.DataGridView()
+        Me.BookID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CheckoutLength = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Title = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Author = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Genre = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.CheckoutPeriod = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Available = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Info = New System.Windows.Forms.DataGridViewButtonColumn()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ISBN = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MoreInfo = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Checkout = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.LibraryDataSet = New Library.LibraryDataSet()
+        Me.ResourcesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ResourcesTableAdapter = New Library.LibraryDataSetTableAdapters.ResourcesTableAdapter()
+        Me.TableAdapterManager = New Library.LibraryDataSetTableAdapters.TableAdapterManager()
+        Me.CheckoutBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CheckoutTableAdapter = New Library.LibraryDataSetTableAdapters.CheckoutTableAdapter()
+        CType(Me.SearchResults, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LibraryDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ResourcesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CheckoutBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'AuthorSearch
         '
-        Me.AuthorSearch.Location = New System.Drawing.Point(848, 132)
+        Me.AuthorSearch.Location = New System.Drawing.Point(765, 137)
         Me.AuthorSearch.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.AuthorSearch.Name = "AuthorSearch"
         Me.AuthorSearch.Size = New System.Drawing.Size(112, 35)
@@ -48,7 +60,7 @@ Partial Class MemberSearch
         '
         'TitleSearch
         '
-        Me.TitleSearch.Location = New System.Drawing.Point(390, 137)
+        Me.TitleSearch.Location = New System.Drawing.Point(362, 137)
         Me.TitleSearch.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.TitleSearch.Name = "TitleSearch"
         Me.TitleSearch.Size = New System.Drawing.Size(112, 35)
@@ -58,7 +70,7 @@ Partial Class MemberSearch
         '
         'AuthorTB
         '
-        Me.AuthorTB.Location = New System.Drawing.Point(580, 137)
+        Me.AuthorTB.Location = New System.Drawing.Point(559, 137)
         Me.AuthorTB.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.AuthorTB.Name = "AuthorTB"
         Me.AuthorTB.Size = New System.Drawing.Size(148, 26)
@@ -74,60 +86,122 @@ Partial Class MemberSearch
         Me.TitleTB.TabIndex = 6
         Me.TitleTB.Text = "Book Title..."
         '
-        'DataGridView1
+        'SearchResults
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Title, Me.Author, Me.Genre, Me.CheckoutPeriod, Me.Available, Me.Info})
-        Me.DataGridView1.Location = New System.Drawing.Point(140, 238)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(850, 346)
-        Me.DataGridView1.TabIndex = 5
+        Me.SearchResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader
+        Me.SearchResults.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+        Me.SearchResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.SearchResults.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.BookID, Me.CheckoutLength, Me.Title, Me.Author, Me.Genre, Me.ISBN, Me.MoreInfo, Me.Checkout})
+        Me.SearchResults.Location = New System.Drawing.Point(140, 213)
+        Me.SearchResults.Name = "SearchResults"
+        Me.SearchResults.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders
+        Me.SearchResults.RowTemplate.Height = 28
+        Me.SearchResults.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.SearchResults.Size = New System.Drawing.Size(737, 350)
+        Me.SearchResults.TabIndex = 10
+        Me.SearchResults.Visible = False
+        '
+        'BookID
+        '
+        Me.BookID.HeaderText = "Resource ID"
+        Me.BookID.Name = "BookID"
+        Me.BookID.Visible = False
+        Me.BookID.Width = 135
+        '
+        'CheckoutLength
+        '
+        Me.CheckoutLength.HeaderText = "Checkout Period"
+        Me.CheckoutLength.Name = "CheckoutLength"
+        Me.CheckoutLength.Visible = False
+        Me.CheckoutLength.Width = 162
         '
         'Title
         '
         Me.Title.HeaderText = "Title"
         Me.Title.Name = "Title"
+        Me.Title.Width = 74
         '
         'Author
         '
         Me.Author.HeaderText = "Author"
         Me.Author.Name = "Author"
+        Me.Author.Width = 93
         '
         'Genre
         '
         Me.Genre.HeaderText = "Genre"
         Me.Genre.Name = "Genre"
+        Me.Genre.Width = 90
         '
-        'CheckoutPeriod
+        'ISBN
         '
-        Me.CheckoutPeriod.HeaderText = "Checkout Period"
-        Me.CheckoutPeriod.Name = "CheckoutPeriod"
+        Me.ISBN.HeaderText = "ISBN"
+        Me.ISBN.Name = "ISBN"
+        Me.ISBN.Width = 83
         '
-        'Available
+        'MoreInfo
         '
-        Me.Available.HeaderText = "Available?"
-        Me.Available.Name = "Available"
+        Me.MoreInfo.HeaderText = "More Info"
+        Me.MoreInfo.Name = "MoreInfo"
+        Me.MoreInfo.Text = "More Info"
+        Me.MoreInfo.Width = 83
         '
-        'Info
+        'Checkout
         '
-        Me.Info.HeaderText = "More Info"
-        Me.Info.Name = "Info"
+        Me.Checkout.HeaderText = "Checkout"
+        Me.Checkout.Name = "Checkout"
+        Me.Checkout.Text = "Checkout"
+        Me.Checkout.Width = 83
+        '
+        'LibraryDataSet
+        '
+        Me.LibraryDataSet.DataSetName = "LibraryDataSet"
+        Me.LibraryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ResourcesBindingSource
+        '
+        Me.ResourcesBindingSource.DataMember = "Resources"
+        Me.ResourcesBindingSource.DataSource = Me.LibraryDataSet
+        '
+        'ResourcesTableAdapter
+        '
+        Me.ResourcesTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CheckoutTableAdapter = Me.CheckoutTableAdapter
+        Me.TableAdapterManager.EmployeesTableAdapter = Nothing
+        Me.TableAdapterManager.MembersTableAdapter = Nothing
+        Me.TableAdapterManager.ResourcesTableAdapter = Me.ResourcesTableAdapter
+        Me.TableAdapterManager.UpdateOrder = Library.LibraryDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'CheckoutBindingSource
+        '
+        Me.CheckoutBindingSource.DataMember = "Checkout"
+        Me.CheckoutBindingSource.DataSource = Me.LibraryDataSet
+        '
+        'CheckoutTableAdapter
+        '
+        Me.CheckoutTableAdapter.ClearBeforeFill = True
         '
         'MemberSearch
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1098, 791)
+        Me.Controls.Add(Me.SearchResults)
         Me.Controls.Add(Me.AuthorSearch)
         Me.Controls.Add(Me.TitleSearch)
         Me.Controls.Add(Me.AuthorTB)
         Me.Controls.Add(Me.TitleTB)
-        Me.Controls.Add(Me.DataGridView1)
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "MemberSearch"
         Me.Text = "MemberSearch"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SearchResults, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LibraryDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ResourcesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CheckoutBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -137,11 +211,19 @@ Partial Class MemberSearch
     Friend WithEvents TitleSearch As Button
     Friend WithEvents AuthorTB As TextBox
     Friend WithEvents TitleTB As TextBox
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents SearchResults As DataGridView
+    Friend WithEvents BookID As DataGridViewTextBoxColumn
+    Friend WithEvents CheckoutLength As DataGridViewTextBoxColumn
     Friend WithEvents Title As DataGridViewTextBoxColumn
     Friend WithEvents Author As DataGridViewTextBoxColumn
     Friend WithEvents Genre As DataGridViewTextBoxColumn
-    Friend WithEvents CheckoutPeriod As DataGridViewTextBoxColumn
-    Friend WithEvents Available As DataGridViewTextBoxColumn
-    Friend WithEvents Info As DataGridViewButtonColumn
+    Friend WithEvents ISBN As DataGridViewTextBoxColumn
+    Friend WithEvents MoreInfo As DataGridViewButtonColumn
+    Friend WithEvents Checkout As DataGridViewButtonColumn
+    Friend WithEvents LibraryDataSet As LibraryDataSet
+    Friend WithEvents ResourcesBindingSource As BindingSource
+    Friend WithEvents ResourcesTableAdapter As LibraryDataSetTableAdapters.ResourcesTableAdapter
+    Friend WithEvents TableAdapterManager As LibraryDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents CheckoutTableAdapter As LibraryDataSetTableAdapters.CheckoutTableAdapter
+    Friend WithEvents CheckoutBindingSource As BindingSource
 End Class
--- Library/Library/MemberSearch.vb
+++ Library/Library/MemberSearch.vb
@@ -1,5 +1,69 @@
 ﻿Public Class MemberSearch
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+    Dim CheckoutPeriod As Long
+    Dim NumberOfRows As Integer
+    Dim CheckedOutRecord As Integer
+    Dim bookInfo As Object
+    Dim resourceID As String
+    Dim ResourceStatus As String
+    Dim DueDate As Date
+    Dim CheckoutDate As Date
+
+    Dim MemberID As String = Login.memberID
+    Dim RowData As Object
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
+
+    Private Sub SearchResults_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles SearchResults.CellContentClick
+        If e.RowIndex < 0 Then
+            Exit Sub
+        End If
+
+        Dim grid = DirectCast(sender, DataGridView)
+
+        If TypeOf grid.Columns(e.ColumnIndex) Is DataGridViewButtonColumn Then
+            If grid.Columns(e.ColumnIndex).Name = "MoreInfo" Then
+                resourceID = SearchResults.Rows(e.RowIndex).Cells(0).Value
+
+                'ResourcesTableAdapter.RenewBookUpdateQuery(currentdate, resourceID)
+
+                bookInfo = CheckoutTableAdapter.GetDataByMemberCheckout(MemberID)(x)
+                resourceID = bookInfo.resourceID
+
+                'CheckoutPeriod = SearchResults.Rows(e.RowIndex).Cells(1).Value
+                'DueDate = DateAdd(DateInterval.Day, CheckoutPeriod, currentdate)
+                'ResourceStatus = "Due " & DueDate.Date
+                'SearchResults.Rows(e.RowIndex).Cells(6).Value = ResourceStatus
+                'SearchResults.Rows(e.RowIndex).Cells(5).Value = currentdate
+
+                MessageBox.Show("you have successfully renewed your book.", " Book Renewal Successful", MessageBoxButtons.OK)
+            End If
+        End If
+
+        If grid.Columns(e.ColumnIndex).Name = "Checkout" Then
+            'resourceID = SearchResults.Rows(e.RowIndex).Cells(0).Value
+            'CheckoutTableAdapter.ReturnBookUpdateQuery(currentdate, resourceID)
+            'SearchResults.Rows(e.RowIndex).Cells(6).Value = currentdate
+
+            MessageBox.Show("you have successfully returned your book.", " Book Return Successful", MessageBoxButtons.OK)
+        End If
+
+        SearchResults.Rows.Clear()
+        'LoadDataGrid()
+    End Sub
+
+    Private Sub ResourcesBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
+        Me.Validate()
+        Me.ResourcesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.LibraryDataSet)
+
+    End Sub
+
+    Private Sub MemberSearch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LibraryDataSet.Checkout' table. You can move, or remove it, as needed.
+        Me.CheckoutTableAdapter.Fill(Me.LibraryDataSet.Checkout)
+        'TODO: This line of code loads data into the 'LibraryDataSet.Resources' table. You can move, or remove it, as needed.
+        Me.ResourcesTableAdapter.Fill(Me.LibraryDataSet.Resources)
 
     End Sub
 End Class
